e58b80320e45a62bd0e816b90fa7e1caf2bb84c9
like-thirll <himanshuinfo95@gmail.com>
2021-01-12 00:04:01 +0530

    #1 closes

---
+++ src/main/java/com/dashboard/automation/TestingResultsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingResultsDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingResultsDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dashboard/automation/TestingResultsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingResultsDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
