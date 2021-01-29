b5a7996e62336140409657a2ae8a4e13574f220f
like-thirll <himanshuinfo95@gmail.com>
2021-01-30 02:52:38 +0530

    Update dashboard  controller

--- src/main/java/com/dashboard/automation/web/controller/Dashboard.java
+++ src/main/java/com/dashboard/automation/web/controller/Dashboard.java
@@ -1,13 +1,37 @@
 package com.dashboard.automation.web.controller;
 
+import com.dashboard.automation.model.RunHistory;
+import com.dashboard.automation.model.Suite;
+import com.dashboard.automation.services.RunHistoryService;
+import com.dashboard.automation.services.SuiteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import java.util.ArrayList;
+
 @RequestMapping("/api/v1/")
 @RestController
+@RequiredArgsConstructor
 public class Dashboard {
+
+    private final RunHistoryService runHistoryService;
+    private final SuiteService suiteService;
+
+    @GetMapping("runHistory")
+    public ResponseEntity<ArrayList<RunHistory>> getRunHistory() {
+        return new ResponseEntity(runHistoryService.getRunHistory(), HttpStatus.OK);
+    }
+
+    @GetMapping("suites/{runId}")
+    public ResponseEntity<ArrayList<Suite>> getSuitesByRunId(@PathVariable @NonNull @Validated Integer runId) {
+        return new ResponseEntity(suiteService.getSuitesByRunId(runId), HttpStatus.OK);
+    }
 
 }
